e02a593b465ec98b11d47b08a6524bc0dce6c8e7
Andrés García <jgarciaf106@gmail.com>
2022-03-29 13:12:37 -0600

    Route query changes

--- src/api/routes.py
+++ src/api/routes.py
@@ -244,7 +244,7 @@ def get_productscategories():
             Product.id, Product.cat_code, Product.description.label("prod_desc")
         )
         .join(
-            Category.query(Category.description.label("cat_desc")),
+            Category,
             Product.cat_code == Category.cat_code,
         )
         .all()
